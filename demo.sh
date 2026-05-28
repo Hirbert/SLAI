4e849e485fdc85a02fcd1a5e395ce506952d1219
Hirbert <912486946@qq.com>
2026-05-28 14:04:39 +0800

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -1,8 +1,8 @@
 docker run --rm -v `pwd`/test_data:/data slai  python3 SLAI.py \
-	--bamfile /data/sample_data/sample20220912-A-A00168-R-20220908-C93_I4.sorted.rmdup.realign.bam  \
+	--bamfile /data/sample_data/P265700-WatchMaker.demo.bam  \
 	--fasta /data/database/ucsc.hg19.fasta \
 	--outdir /data/output \
-	--snv /data/sample_data/sample20220912-A-A00168-R-20220908-C93_I4.final.fp_filter.txt \
+	--snv /data/sample_data/P265700-WatchMaker.snv.chr1.txt \
 	--flank 300 \
 	--cpu 8 \
-	--outfile sample20220912-A-A00168-R-20220908-C93_I4
+	--outfile P265700-WatchMaker.chr1
